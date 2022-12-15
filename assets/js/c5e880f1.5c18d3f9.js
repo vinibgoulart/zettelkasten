b0d83937c2d120ac65aa8851711d4bc80a075b99
vinibgoulart <vinibgoulart@users.noreply.github.com>
2022-12-15 14:22:51 +0000

    deploy: 7799e23cdbce079487de57009ca3d1cf6ec9826f

---
+++ assets/js/c5e880f1.5c18d3f9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblazettelkasten=self.webpackChunkblazettelkasten||[]).push([[940],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
